f259d3637d063576593d3cf0ea17fbca2c17f299
Zuzka Gregorova <93294795+Zuzanagrego@users.noreply.github.com>
2022-07-26 15:20:57 -0500

    Searchbar now fully functional

--- index.js
+++ index.js
@@ -8,28 +8,48 @@ const searchURL =
   "https://api.themoviedb.org/3/search/movie?api_key=3a6a4801c4e1409a0655b5db3e583c55&query=";
 
 const form = document.querySelector(".form");
-const search = document.querySelector(".search");
+const search = document.querySelector(".search__field");
 const main = document.querySelector(".main");
 
 async function getMovies(url) {
   const movies = await fetch(url);
   const moviesData = await movies.json();
-  console.log(moviesData);
-  const userListEl = document.querySelector(".movie-list");
-  userListEl.innerHTML = moviesData.results
+  const movieListEl = document.querySelector(".movie-list");
+  movieListEl.innerHTML = moviesData.results
     .map((movie) => moviesHTML(movie))
     .join("");
+
+  /*
+    ONLY DISPLAY BETTER THAN 7.5
+            const rating = moviesData.results.map((movie) => {
+            return movie.vote_average;
+          });
+    */
 }
 
 getMovies(popularURL);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const searchValue = search.value;
-  if (searchValue !== "") {
+  let searchValue = search.value;
+  console.log(searchValue);
+  if (searchValue && searchValue !== "") {
     getMovies(searchURL + searchValue);
+    searchValue = "";
+  } else {
+    window.location.reload();
   }
 });
+
+function slideToMovies() {
+  document.querySelector(".main").scrollIntoView();
+}
+
+function displayByRating() {
+  if (rating > 7.5) {
+    return "display-rating";
+  }
+}
 
 function moviesHTML(movie) {
   return `<div class="movie-card" onclick="showUserPost()">
